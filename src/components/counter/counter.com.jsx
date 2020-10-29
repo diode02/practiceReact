f7a65d45156b509126fa573b5063f7df5164944d
Muhammad Waqas Ilyas <diode02@hotmail.com>
2020-10-29 18:37:57 +0500

    till counter with button

---
+++ src/components/counter/counter.com.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const Couter = () => {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Pakistan");
+
+  const handleDecrease = () => {
+    setCount(count - 1);
+  };
+
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const handleMultiply = () => {
+    setCount(count * 10);
+  };
+
+  return (
+    <div>
+      this site is created in {count > 3 ? <h1>India</h1> : <h1>{name}</h1>}
+      <br />
+      {count}
+      <br />
+      <button onClick={handleIncrease}>increase</button>
+      <br />
+      <button onClick={handleDecrease}>decrease</button>
+      <br />
+      <button onClick={handleMultiply}>multi</button>
+    </div>
+  );
+};
+
+export default Couter;
