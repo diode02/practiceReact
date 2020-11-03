e614006bab66d43ad210f7b76f17fc2bdf832935
Muhammad Waqas Ilyas <diode02@hotmail.com>
2020-11-03 15:40:17 +0500

    added forms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 const { default: Couter } = require("./components/counter/counter.com");
+const { default: UserinputPage } = require("./pages/userinput/user-input.com");
 
 function App() {
   return (
     <div className="App">
-      <Couter />
+      {/* <Couter /> */}
+      <UserinputPage />
     </div>
   );
 }
---
+++ src/pages/userinput/user-input.com.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const UserinputPage = () => {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (name.length < 5) {
+      alert("name value must be gretaer then 5");
+      return;
+    }
+    //send this name to backend
+    setName("");
+  };
+  return (
+    <div>
+      {/* <span>{name}</span> */}
+      <form>
+        <label> Name</label>
+        <input
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+          value={name}
+        />
+
+        <span onClick={handleSubmit}> OK</span>
+      </form>
+    </div>
+  );
+};
+
+export default UserinputPage;
