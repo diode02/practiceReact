56f77e956ffcd5d60e500af73ae01bb0dff669bf
Shaoors <shaoors16.github.com>
2020-11-05 03:42:00 +0500

    Calculator with field

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import CalWithForm from "./pages/cal-with-form/cal-with-form.com";
-
+// const { default: Couter } = require("./components/counter/counter.com");
+// const {
+// default: CalculatorPage,
+// } = require("./pages/calculator/calculator.com");
+// const { default: UserinputPage } = require("./pages/userinput/user-input.com");
+import CalWithform from "./pages/cal-with-form/cal-with-form.com";
 function App() {
   return (
     <div className="App">
-      <CalWithForm />
+      <CalWithform />
     </div>
   );
 }
